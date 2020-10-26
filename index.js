1ad552ead876976bb2d8f574313172d451096dd3
Madison Stanford <madison.stanford@gmail.com>
2020-10-25 19:18:47 -0700

    Configure for local testing

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('newrelic');
 
 const app = express();
 
@@ -8,13 +9,17 @@ const HOST = 'localhost';
 app.use(express.static(__dirname));
 
 app.get('/info', (req, res, next) => {
-  res.send('This is a proxy service which proxies to the 4 services present in the Team 2 FEC project.');
+  res.send('This is a proxy service which proxies to the 4 services present in the Team 1 SDC project.');
 });
 
 app.get('/*', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(PORT, HOST, () => {
-  console.log(`Starting Proxy at ${HOST}:${PORT}`);
+app.post('/*', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(PORT, () => {
+  console.log(`Proxy running on ${HOST}:${PORT}`);
 });
